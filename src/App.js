562475c535cec9242d4ef0dd74357e2956205602
zhoujing01 <zhoujing01@jsfund.cn>
2023-06-15 13:50:32 +0800

    js init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+
+function App(props) {
+  return (
+    <View></View>
+  );
+}
+App.defaultProps={};
+App.propTypes={};
+export default App;
