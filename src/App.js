ac62e07be6450f03fb412e0db98a32d9919a867d
zhoujing01 <zhoujing01@jsfund.cn>
2023-06-15 16:19:58 +0800

    feat: swipable view  in terms of ScrollView

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Dimensions, ScrollView, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
+
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+const WIDTH = Dimensions.get('window').width;
 
 function App(props) {
   return (
-    <View></View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <ScrollView style={{
+          backgroundColor: '#F4F5F6'
+        }}
+          horizontal={true}
+          pagingEnabled={true}
+          showsHorizontalScrollIndicator={false}
+        >
+          <View style={{
+            flexDirection: 'row'
+          }}>
+            <View style={{
+              width: WIDTH,
+              height: 500,
+              backgroundColor: 'red'
+            }}></View>
+            <View style={{
+              width: WIDTH,
+              height: 500,
+              backgroundColor: 'purple'
+            }}></View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-App.defaultProps={};
-App.propTypes={};
+App.defaultProps = {};
+App.propTypes = {};
 export default App;
