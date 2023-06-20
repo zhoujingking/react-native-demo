29981b562132900fcf2857b1442acabfe286210f
zhoujing01 <zhoujing01@jsfund.cn>
2023-06-20 15:14:19 +0800

    RN-reanimated: basic animation

--- src/App.js
+++ src/App.js
@@ -1,56 +1,108 @@
-import React, { useEffect, useRef } from 'react';
-import { Animated, Dimensions, ScrollView, Text, View } from 'react-native';
+import React, { forwardRef, useEffect, useRef } from 'react';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import AnimationMovement from './animation/move';
-import AnimationRotation from './animation/rotate';
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withRepeat, withSequence, withSpring, withTiming } from 'react-native-reanimated';
 
-const FadeView = props => {
-  const fadeValue = useRef(new Animated.Value(0)).current;
+import { Circle, Svg } from 'react-native-svg';
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+
+function CircleTest() {
+  const sv = useSharedValue('5%');
 
   useEffect(() => {
-    Animated.timing(fadeValue, {
-      toValue: 1,
-      duration: 10000,
-      useNativeDriver: true
-    }).start();
-  }, [fadeValue]);
+    sv.value = withRepeat(withTiming('50%', {
+      duration: 5000
+    }), -1, true);
+  }, []);
 
   return (
-    <Animated.View style={{
-      opacity: fadeValue
-    }}>
-      {
-        props.children
-      }
-    </Animated.View>
-  )
-};
+    <Svg height="200" width="200" style={{backgroundColor: 'cyan'}}>
+      <AnimatedCircle cx="50%" cy="50%" fill="purple" r={sv} />
+    </Svg>
+  );
+}
 
 function App(props) {
+  const angle = useSharedValue(0);
+  const offset = useSharedValue(0);
+  const sprinOffset = useSharedValue(-100);
+  const rotateAniStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotate: `${angle.value}deg`
+        }
+      ]
+    }
+  });
+
+  const moveAniStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: offset.value
+        }
+      ]
+    }
+  });
+
+  const sprinAniStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: sprinOffset.value
+        }
+      ]
+    }
+  })
+
+  useEffect(() => {
+    angle.value = withRepeat(withTiming(720, {
+      duration: 5000,
+      easing: Easing.circle
+    }), -1, false);
+
+    offset.value = withRepeat(withSequence(
+      withTiming(-100, {
+        duration: 1000
+      }),
+      withTiming(100, {
+        duration: 1000
+      }),
+    ), -1, true);
+
+    sprinOffset.value = withRepeat(
+      withSpring(100)
+      , -1, true)
+  }, []);
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{
         flex: 1,
-        // justifyContent: 'center',
-        // alignItems: 'center'
+        justifyContent: 'center',
+        alignItems: 'center',
+        rowGap: 20
       }}>
-        <AnimationMovement>
-        </AnimationMovement>
-        <AnimationRotation style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'yellow'
-        }}>
-          <Text style={{
-            backgroundColor: 'red'
-          }}>GODKING</Text>
-        </AnimationRotation>
+        <Animated.View style={[styles.box, rotateAniStyle]}></Animated.View>
+        <Animated.View style={[styles.box, moveAniStyle]}></Animated.View>
+        <Animated.View style={[styles.box, sprinAniStyle]}></Animated.View>
+        <CircleTest />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#2A66F6'
+  }
+})
+
 App.defaultProps = {};
 App.propTypes = {};
 export default App;
